9db2fe650bc34561147f2da8f6f6727e71140724
Pratyush <pratyushjain1916@gmail.com>
2023-09-18 23:39:06 +0530

    board styling

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Board.css'
+import { MoreHorizontal } from 'react-feather'
 
 function Board() {
   return (
     <div className='board'>
-        <h1>Board</h1>
+      <div className='board-top'>
+        <p className="board-top-title">
+        To Do  <span>2</span>
+        </p>
+        <p><MoreHorizontal /></p>
+      </div>
+      <div className="board-cards">
+        <h1>Card 1</h1>
+        <h1>Card 2</h1>
+      </div>
     </div>
   )
 }
